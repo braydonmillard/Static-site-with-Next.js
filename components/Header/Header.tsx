c81cb20461e1ff61d6d9fd259abef6c05116153f
braydonmillard <46946203+braydonmillard@users.noreply.github.com>
2022-07-15 20:20:27 -0400

    first commit

---
+++ components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Link from 'next/link';
+import { Center } from '../Center';
+import { Container, Logo } from './style';
+
+export const Header = () => {
+  return (
+    <Container>
+      <Center>
+        <Logo>
+          <Link href='/'>
+            <a>What's Next?!</a>
+          </Link>
+        </Logo>
+      </Center>
+    </Container>
+  );
+};
