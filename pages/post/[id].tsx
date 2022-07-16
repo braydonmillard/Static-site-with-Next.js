1d46ef6e29fc4e3a50091d6f0d95a8356766deb5
braydonmillard <46946203+braydonmillard@users.noreply.github.com>
2022-07-16 16:44:19 -0400

    Redux implemented

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,32 +1,46 @@
 import React from 'react';
-
-import { GetStaticProps } from 'next';
-import { useRouter } from 'next/router';
-import { fetchPost } from '../../api/post';
-import { Post as PostType } from '../../shared/types';
+import { NextPage } from 'next';
+import { useSelector } from 'react-redux';
 import { Loader } from '../../components/Loader';
-import { postPaths as paths } from '../../shared/staticPaths';
 import { PostBody } from '../../components/Post/PostBody';
+import { Comments } from '../../components/Comments';
 
-type PostProps = {
-  post: PostType;
-};
+import { fetchPost } from '../../api/post';
+import { fetchComments } from '../../api/comments/fetch';
+import { State, store } from '../../store';
+import { PostState, UPDATE_POST_ACTION } from '../../store/post';
+import { CommentsState, UPDATE_COMMENTS_ACTION } from '../../store/comments';
 
-export const getStaticProps: GetStaticProps<PostProps> = async ({ params }) => {
-  if (typeof params.id !== 'string') throw new Error('Unexpected id');
-  const post = await fetchPost(params.id);
-  return { props: { post } };
-};
+export const getServerSideProps = store.getServerSideProps(
+  (store) =>
+    async ({ params }) => {
+      if (typeof params.id !== 'string') {
+        throw new Error('Unexpected id');
+      }
 
-export async function getStaticPaths() {
-  return { paths, fallback: true };
-}
+      const comments = await fetchComments(params.id);
+      const post = await fetchPost(params.id);
 
-const Post = ({ post }: PostProps) => {
-  const router = useRouter();
+      store.dispatch({ type: UPDATE_POST_ACTION, post });
+      store.dispatch({ type: UPDATE_COMMENTS_ACTION, comments });
 
-  if (router.isFallback) return <Loader />;
-  return <PostBody post={post} />;
+      return null;
+    }
+);
+
+const Post: NextPage = () => {
+  const post = useSelector<State, PostState>(({ post }) => post);
+  const comments = useSelector<State, CommentsState>(
+    ({ comments }) => comments
+  );
+
+  if (!post) return <Loader />;
+  return (
+    <>
+      <PostBody post={post} />
+      <Comments comments={comments} post={post.id} />
+    </>
+  );
 };
 
 export default Post;
